1bbe40ee6acb618f4283f8fb5e49f19efc2f6fc2
piotrmizerka <piotrmizerka@gmail.com>
2025-01-05 00:04:36 +0100

    relations for double indexed x's
    not launched yet!

--- scripts/SP_N_Steinberg.jl
+++ scripts/SP_N_Steinberg.jl
@@ -34,8 +34,8 @@ function extended_f_sp_2n(n::Integer)
         [(:X,i,j) for (i,j) in ordered_pairs],
         [(:Y,i,j) for (i,j) in unordered_pairs],
         [(:Yt,i,j) for (i,j) in unordered_pairs],
-        [(:Z,i,j) for (i,j) in unordered_pairs],
-        [(:Zt,i,j) for (i,j) in unordered_pairs],
+        [(:Z,i,j) for (i,j) in ordered_pairs],
+        [(:Zt,i,j) for (i,j) in ordered_pairs],
     )
 
     gens_number = div(length(x_y_z_gens),2)
@@ -75,6 +75,207 @@ end
 
 quotient_hom = let source = Extended_f_sp_2n, target = Sp_N
     Groups.Homomorphism((i, F, G) -> quotient_hom_gens(i, F,G ), source, target)
+end
+
+function relations_St(
+    F_G::Groups.FreeGroup,
+    N::Integer;
+    sq_adj_op = "all"
+)
+    gen_dict = Dict(LowCohomologySOS.determine_letter(gens(F_G, i)) => gens(F_G, i) for i in eachindex(S))
+
+    range_as_list = [i for i in 1:N]
+    pairs = [(i,j) for i ∈ 1:n for j ∈ deleteat!(copy(range_as_list), findall(j->j==i,copy(range_as_list)))]
+    triples = [(i,j,k) for i ∈ range_as_list
+                    for j ∈ deleteat!(copy(range_as_list), findall(j->j==i,copy(range_as_list))) 
+                    for k ∈ deleteat!(copy(range_as_list), findall(k->k∈[i,j],copy(range_as_list)))]
+    quadruples = [(i,j,k,l) for i ∈ range_as_list
+                        for j ∈ deleteat!(copy(range_as_list), findall(j->j==i,copy(range_as_list))) 
+                        for k ∈ deleteat!(copy(range_as_list), findall(k->k∈[i,j],copy(range_as_list)))
+                        for l ∈ deleteat!(copy(range_as_list), findall(l->l∈[i,j,k],copy(range_as_list)))]
+    
+    x(i,j) = gen_dict[(:x,i,j)]
+    y(i,j) = gen_dict[(:y,min(i,j),max(i,j))]
+    yt(i,j) = gen_dict[(:yt,min(i,j),max(i,j))]
+    z(i,j) = gen_dict[(:z,i,j)]
+    zt(i,j) = gen_dict[(:zt,i,j)]
+
+    relations_sq = vcat(
+        [z(i,j)*zt(i,j)^(-1)*z(i,j)*z(i,j)*zt(i,j)^(-1)*z(i,j)*z(i,j)*zt(i,j)^(-1)*z(i,j)*z(i,j)*zt(i,j)^(-1)*z(i,j) for (i,j) in pairs],
+
+        [com(x(i,j),y(i,j))*z(i,j)^(-2) for (i,j) in pairs],
+        [com(x(i,j),yt(i,j))*zt(j,i)^2 for (i,j) in pairs],
+        [com(x(i,j),z(j,i))*(y(i,j)*z(i,j))^(-1) for (i,j) in pairs],
+        [com(x(i,j),z(j,i))*(z(i,j)*y(i,j))^(-1) for (i,j) in pairs],
+        [com(z(i,j),y(i,j)) for (i,j) in pairs],
+        [com(x(i,j),zt(i,j))*yt(i,j)*zt(j,i)^(-1) for (i,j) in pairs],
+        [com(x(i,j),zt(i,j))*zt(j,i)^(-1)*yt(i,j) for (i,j) in pairs],
+        [com(zt(j,i),yt(i,j)^(-1)) for (i,j) in pairs],
+        [com(y(i,j),zt(i,j))*z(j,i)*x(j,i)^(-1) for (i,j) in pairs],
+        [com(y(i,j),zt(i,j))*x(j,i)^(-1)*z(j,i) for (i,j) in pairs],
+        [com(x(j,i),z(j,i)^(-1)) for (i,j) in pairs],
+        [com(yt(i,j), z(i,j))*zt(j,i)*x(i,j) for (i,j) in pairs],
+        [com(yt(i,j), z(i,j))*x(i,j)*zt(j,i) for (i,j) in pairs],
+        [com(x(i,j),zt(j,i)) for (i,j) in pairs],
+
+        [com(x(i,j), z(i,j)) for (i,j) in pairs],
+        [com(z(i,j), x(i,j)) for (i,j) in pairs],
+        [com(x(i,j), zt(j,i)) for (i,j) in pairs],
+        [com(zt(j,i), x(i,j)) for (i,j) in pairs],
+        [com(y(i,j), z(j,i)) for (i,j) in pairs],
+        [com(z(j,i), y(i,j)) for (i,j) in pairs],
+        [com(yt(i,j), zt(j,i)) for (i,j) in pairs],
+        [com(zt(j,i), yt(i,j)) for (i,j) in pairs],
+        [com(y(i,j), z(i,j)) for (i,j) in pairs],
+        [com(z(i,j), y(i,j)) for (i,j) in pairs],
+        [com(yt(i,j), zt(i,j)) for (i,j) in pairs],
+        [com(zt(i,j), yt(i,j)) for (i,j) in pairs],
+    )
+
+    relations_adj = vcat(
+        [com(x(i,j),x(j,k))*x(i,k)^(-1) for (i,j,k) in triples],
+        [com(x(i,j),y(j,k))*y(i,k)^(-1) for (i,j,k) in triples],
+        [com(x(i,j),yt(i,k))*yt(j,k) for (i,j,k) in triples],
+        
+        [com(x(i,j),y(i,j))*z(i,k)^(-2) for (i,j,k) in triples],
+        [com(x(i,j),yt(i,j))*zt(j,k)^2 for (i,j,k) in triples],
+        [com(x(i,j),z(j,k))*(y(i,j)*z(i,k))^(-1) for (i,j,k) in triples],
+        [com(x(i,j),z(j,k))*(z(i,k)*y(i,j))^(-1) for (i,j,k) in triples],
+        [com(z(i,k),y(i,j)) for (i,j,k) in triples],
+        [com(x(i,j),zt(i,k))*yt(i,j)*zt(j,k)^(-1) for (i,j,k) in triples],
+        [com(x(i,j),zt(i,k))*zt(j,k)^(-1)*yt(i,j) for (i,j,k) in triples],
+        [com(zt(j,k),yt(i,j)^(-1)) for (i,j,k) in triples],
+        [com(y(i,j),zt(i,k))*z(j,k)*x(j,i)^(-1) for (i,j,k) in triples],
+        [com(y(i,j),zt(i,k))*x(j,i)^(-1)*z(j,k) for (i,j,k) in triples],
+        [com(x(j,i),z(j,k)^(-1)) for (i,j,k) in triples],
+        [com(yt(i,j), z(i,k))*zt(j,k)*x(i,j) for (i,j,k) in triples],
+        [com(yt(i,j), z(i,k))*x(i,j)*zt(j,k) for (i,j,k) in triples],
+        [com(x(i,j),zt(j,k)) for (i,j,k) in triples],
+
+        [com(x(i,j), z(i,k)) for (i,j,k) in triples],
+        [com(z(i,k), x(i,j)) for (i,j,k) in triples],
+        [com(x(i,j), zt(j,k)) for (i,j,k) in triples],
+        [com(zt(j,k), x(i,j)) for (i,j,k) in triples],
+        [com(y(i,j), z(j,k)) for (i,j,k) in triples],
+        [com(z(j,k), y(i,j)) for (i,j,k) in triples],
+        [com(yt(i,j), zt(j,k)) for (i,j,k) in triples],
+        [com(zt(j,k), yt(i,j)) for (i,j,k) in triples],
+        [com(y(i,j), z(i,k)) for (i,j,k) in triples],
+        [com(z(i,k), y(i,j)) for (i,j,k) in triples],
+        [com(yt(i,j), zt(i,k)) for (i,j,k) in triples],
+        [com(zt(i,k), yt(i,j)) for (i,j,k) in triples],
+
+        [com(x(i,j), x(i,k)) for (i,j,k) in triples],
+        [com(x(i,j), x(k,j)) for (i,j,k) in triples],
+        [com(x(i,j), y(i,k)) for (i,j,k) in triples],
+        [com(y(i,k), x(i,j)) for (i,j,k) in triples],
+        [com(x(i,j), yt(k,j)) for (i,j,k) in triples],
+        [com(yt(k,j), x(i,j)) for (i,j,k) in triples],
+        [com(y(i,j), y(j,k)) for (i,j,k) in triples],
+        [com(y(i,j), y(i,k)) for (i,j,k) in triples],
+        [com(y(i,j), y(k,j)) for (i,j,k) in triples],
+        [com(yt(i,j), yt(j,k)) for (i,j,k) in triples],
+        [com(yt(i,j), yt(j,k)) for (i,j,k) in triples],
+        [com(yt(i,j), yt(j,k)) for (i,j,k) in triples],
+
+        [com(z(i,j), y(j,k)) for (i,j,k) in triples],
+        [com(z(i,k), y(j,k)) for (i,j,k) in triples],
+        [com(y(j,k), z(i,j)) for (i,j,k) in triples],
+        [com(y(j,k), z(i,k)) for (i,j,k) in triples],
+        [com(z(i,j), yt(j,k)) for (i,j,k) in triples],
+        [com(z(i,k), yt(j,k)) for (i,j,k) in triples],
+        [com(yt(j,k), z(i,j)) for (i,j,k) in triples],
+        [com(yt(j,k), z(i,k)) for (i,j,k) in triples],
+        [com(z(i,j), x(j,k)) for (i,j,k) in triples],
+        [com(z(i,k), x(j,k)) for (i,j,k) in triples],
+        [com(x(j,k), z(i,j)) for (i,j,k) in triples],
+        [com(x(j,k), z(i,k)) for (i,j,k) in triples],
+        [com(zt(i,j), y(j,k)) for (i,j,k) in triples],
+        [com(zt(i,k), y(j,k)) for (i,j,k) in triples],
+        [com(y(j,k), zt(i,j)) for (i,j,k) in triples],
+        [com(y(j,k), zt(i,k)) for (i,j,k) in triples],
+        [com(zt(i,j), yt(j,k)) for (i,j,k) in triples],
+        [com(zt(i,k), yt(j,k)) for (i,j,k) in triples],
+        [com(yt(j,k), zt(i,j)) for (i,j,k) in triples],
+        [com(yt(j,k), zt(i,k)) for (i,j,k) in triples],
+        [com(zt(i,j), x(j,k)) for (i,j,k) in triples],
+        [com(zt(i,k), x(j,k)) for (i,j,k) in triples],
+        [com(x(j,k), zt(i,j)) for (i,j,k) in triples],
+        [com(x(j,k), zt(i,k)) for (i,j,k) in triples],
+
+        [com(z(i,j), y(j,k)) for (i,j,k) in triples],
+        [com(z(i,k), y(j,k)) for (i,j,k) in triples],
+        [com(y(j,k), z(i,j)) for (i,j,k) in triples],
+        [com(y(j,k), z(i,k)) for (i,j,k) in triples],
+        [com(z(i,j), yt(j,k)) for (i,j,k) in triples],
+        [com(z(i,k), yt(j,k)) for (i,j,k) in triples],
+        [com(yt(j,k), z(i,j)) for (i,j,k) in triples],
+        [com(yt(j,k), z(i,k)) for (i,j,k) in triples],
+        [com(z(i,j), x(j,k)) for (i,j,k) in triples],
+        [com(z(i,k), x(j,k)) for (i,j,k) in triples],
+        [com(x(j,k), z(i,j)) for (i,j,k) in triples],
+        [com(x(j,k), z(i,k)) for (i,j,k) in triples],
+        [com(zt(i,j), y(j,k)) for (i,j,k) in triples],
+        [com(zt(i,k), y(j,k)) for (i,j,k) in triples],
+        [com(y(j,k), zt(i,j)) for (i,j,k) in triples],
+        [com(y(j,k), zt(i,k)) for (i,j,k) in triples],
+        [com(zt(i,j), yt(j,k)) for (i,j,k) in triples],
+        [com(zt(i,k), yt(j,k)) for (i,j,k) in triples],
+        [com(yt(j,k), zt(i,j)) for (i,j,k) in triples],
+        [com(yt(j,k), zt(i,k)) for (i,j,k) in triples],
+        [com(zt(i,j), x(j,k)) for (i,j,k) in triples],
+        [com(zt(i,k), x(j,k)) for (i,j,k) in triples],
+        [com(x(j,k), zt(i,j)) for (i,j,k) in triples],
+        [com(x(j,k), zt(i,k)) for (i,j,k) in triples]
+    )
+
+    relations_op = vcat(
+        [com(z(i,l), y(j,k)) for (i,j,k,l) in quadruples],
+        [com(y(j,k), z(i,l)) for (i,j,k,l) in quadruples],
+        [com(z(i,l), yt(j,k)) for (i,j,k,l) in quadruples],
+        [com(yt(j,k), z(i,l)) for (i,j,k,l) in quadruples],
+        [com(z(i,l), x(j,k)) for (i,j,k,l) in quadruples],
+        [com(x(j,k), z(i,l)) for (i,j,k,l) in quadruples],
+        [com(zt(i,l), y(j,k)) for (i,j,k,l) in quadruples],
+        [com(y(j,k), zt(i,l)) for (i,j,k,l) in quadruples],
+        [com(zt(i,l), yt(j,k)) for (i,j,k,l) in quadruples],
+        [com(yt(j,k), zt(i,l)) for (i,j,k,l) in quadruples],
+        [com(zt(i,l), x(j,k)) for (i,j,k,l) in quadruples],
+        [com(x(j,k), zt(i,l)) for (i,j,k,l) in quadruples],
+
+        [com(x(i,j), x(k,l)) for (i,j,k,l) in quadruples],
+        [com(x(i,j), y(k,l)) for (i,j,k,l) in quadruples],
+        [com(y(k,l), x(i,j)) for (i,j,k,l) in quadruples],
+        [com(x(i,j), yt(k,l)) for (i,j,k,l) in quadruples],
+        [com(yt(k,l), x(i,j)) for (i,j,k,l) in quadruples],
+        [com(y(i,j), y(k,l)) for (i,j,k,l) in quadruples],
+        [com(y(i,j), yt(k,l)) for (i,j,k,l) in quadruples],
+        [com(yt(k,l), y(i,j)) for (i,j,k,l) in quadruples],
+        [com(yt(i,j), yt(k,l)) for (i,j,k,l) in quadruples],
+
+        [com(z(i,l), y(j,k)) for (i,j,k,l) in quadruples],
+        [com(y(j,k), z(i,l)) for (i,j,k,l) in quadruples],
+        [com(z(i,l), yt(j,k)) for (i,j,k,l) in quadruples],
+        [com(yt(j,k), z(i,l)) for (i,j,k,l) in quadruples],
+        [com(z(i,l), x(j,k)) for (i,j,k,l) in quadruples],
+        [com(x(j,k), z(i,l)) for (i,j,k,l) in quadruples],
+        [com(zt(i,l), y(j,k)) for (i,j,k,l) in quadruples],
+        [com(y(j,k), zt(i,l)) for (i,j,k,l) in quadruples],
+        [com(zt(i,l), yt(j,k)) for (i,j,k,l) in quadruples],
+        [com(yt(j,k), zt(i,l)) for (i,j,k,l) in quadruples],
+        [com(zt(i,l), x(j,k)) for (i,j,k,l) in quadruples],
+        [com(x(j,k), zt(i,l)) for (i,j,k,l) in quadruples]
+    )
+
+    if sq_adj_op == "sq" 
+        return relations_sq
+    elseif sq_adj_op == "adj"
+        return relations_adj
+    elseif sq_adj_op == "op"
+        return relations_op
+    elseif sq_adj_op == "all"
+        return vcat(relations_sq, relations_adj, relations_op)
+    end
 end
 
 # below code to change
